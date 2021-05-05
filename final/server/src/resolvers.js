230a7f9f673ff9c73859b0f9ad24a43cc0e22c72
Michelle Mabuyo <michelle.mabuyo@gmail.com>
2021-05-05 15:21:20 -0400

    refactor: switch from then/catch pattern to try/catch for better readability

--- final/server/src/resolvers.js
+++ final/server/src/resolvers.js
@@ -17,25 +17,23 @@ const resolvers = {
   },
   Mutation: {
     // increments a track's numberOfViews property
-    incrementTrackViews: (_, { id }, { dataSources }) => {
-      return dataSources.trackAPI
-        .incrementTrackViews(id)
-        .then((track) => {
-          return {
-            code: '200',
-            success: true,
-            message: `Successfully incremented number of views for track ${id}`,
-            track,
-          };
-        })
-        .catch((err) => {
-          return {
-            code: err.extensions.response.status,
-            success: false,
-            message: err.extensions.response.statusText,
-            track: null,
-          };
-        });
+    incrementTrackViews: async (_, { id }, { dataSources }) => {
+      try {
+        const track = await dataSources.trackAPI.incrementTrackViews(id);
+        return {
+          code: '200',
+          success: true,
+          message: `Successfully incremented number of views for track ${id}`,
+          track,
+        };
+      } catch (err) {
+        return {
+          code: err.extensions.response.status,
+          success: false,
+          message: err.extensions.response.statusText,
+          track: null,
+        };
+      }
     },
   },
   Track: {
