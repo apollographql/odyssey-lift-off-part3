a9c6c7e920aa052c4fef451f39be8b411b777670
Liz Hennessy <lizhennessy@Lizs-MBP.hsd1.ut.comcast.net>
2023-06-22 21:05:44 -0600

    try out omit

--- client/src/containers/track-card.tsx
+++ client/src/containers/track-card.tsx
@@ -8,7 +8,7 @@ import type { Track } from '../__generated__/graphql'
  * Track Card component renders basic info in a card format
  * for each track populating the tracks grid homepage.
  */
-const TrackCard: React.FC<{ track: Track }> = ({ track }) => {
+const TrackCard: React.FC<{ track: Omit<Track, "modules"> }> = ({ track }) => {
   const { title, thumbnail, author, length, modulesCount } = track;
 
   return (
