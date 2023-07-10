60d4f18e8f7a2ddde5f7663cb892bb043668d8f7
Liz Hennessy <lizhennessy@Lizs-MBP.hsd1.ut.comcast.net>
2023-07-10 09:46:50 -0600

    add import for gql

--- final/client/src/pages/__tests__/tracks.js
+++ final/client/src/pages/__tests__/tracks.js
@@ -1,7 +1,7 @@
 import React from 'react';
 // this adds custom jest matchers from jest-dom
 import '@testing-library/jest-dom/extend-expect';
-import { InMemoryCache } from '@apollo/client';
+import { InMemoryCache, gql } from '@apollo/client';
 import { renderApolloWithRouter, cleanup, waitForElement } from '../../utils/test-utils';
 import Tracks from '../tracks';
 
