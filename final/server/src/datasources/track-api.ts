997b057468b902a55436b1d2aaf40b111da37adc
Liz Hennessy <lizhennessy@Lizs-MBP.hsd1.ut.comcast.net>
2023-07-10 20:59:00 -0600

    update starter and final code to include models and correct context type

--- final/server/src/datasources/track-api.ts
+++ final/server/src/datasources/track-api.ts
@@ -1,21 +1,22 @@
 import { RESTDataSource } from "@apollo/datasource-rest";
+import { TrackModel, AuthorModel, ModuleModel } from "../models"
 
 export class TrackAPI extends RESTDataSource {
   baseURL = "https://odyssey-lift-off-rest-api.herokuapp.com/";
 
   getTracksForHome() {
-    return this.get("tracks");
+    return this.get<TrackModel[]>("tracks");
   }
 
   getAuthor(authorId: string) {
-    return this.get(`author/${authorId}`);
+    return this.get<AuthorModel>(`author/${authorId}`);
   }
 
   getTrack(trackId: string) {
-    return this.get(`track/${trackId}`);
+    return this.get<TrackModel>(`track/${trackId}`);
   }
 
   getTrackModules(trackId: string) {
-    return this.get(`track/${trackId}/modules`);
+    return this.get<ModuleModel[]>(`track/${trackId}/modules`);
   }
 }
